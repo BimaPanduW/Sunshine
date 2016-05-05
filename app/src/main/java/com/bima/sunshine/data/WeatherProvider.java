c26b234fe9ae245e3d9ebb6007cad382004feaea
Bima Pandu Wiguna <bima.p@go-jek.com>
2016-05-05 23:28:06 +0700

    FILL buildUriMatcher

--- app/src/main/java/com/bima/sunshine/data/WeatherProvider.java
+++ app/src/main/java/com/bima/sunshine/data/WeatherProvider.java
@@ -100,15 +100,14 @@ public class WeatherProvider extends ContentProvider {
         testUriMatcher test within TestUriMatcher.
      */
     static UriMatcher buildUriMatcher() {
-        // 1) The code passed into the constructor represents the code to return for the root
-        // URI.  It's common to use NO_MATCH as the code for this case. Add the constructor below.
 
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-        // 2) Use the addURI function to match each of the types.  Use the constants from
-        // WeatherContract to help define the types to the UriMatcher.
+        uriMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER, WEATHER);
+        uriMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*", WEATHER_WITH_LOCATION);
+        uriMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*/#", WEATHER_WITH_LOCATION_AND_DATE);
+        uriMatcher.addURI(WeatherContract.CONTENT_AUTHORITY, WeatherContract.PATH_WEATHER + "/*", LOCATION);
 
-
-        // 3) Return the new matcher!
         return null;
     }
 
