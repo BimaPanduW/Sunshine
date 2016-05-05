fb9bdd944a04659a0b97023432bfba01e40c335b
Bima Pandu Wiguna <bima.p@go-jek.com>
2016-05-06 03:59:15 +0700

    add class FetchWeatherTask

--- app/src/main/java/com/bima/sunshine/ForecastFragment.java
+++ app/src/main/java/com/bima/sunshine/ForecastFragment.java
@@ -100,16 +100,13 @@ public class ForecastFragment extends Fragment {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String location = preferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
         unitType = preferences.getString(getString(R.string.pref_units_key), getString(R.string.pref_units_metric));
-        new FetchWeatherTask().execute(location);
+        new FetchWeatherTask(getActivity(), arrayAdapter).execute(location);
     }
 
     private String getBaseUrl() {
         return "http://api.openweathermap.org/data/2.5/forecast/daily?";
     }
 
-    private String getAppId() {
-        return "b92f80be5981262933e52d95fb637140";
-    }
 
     private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
             throws JSONException {
@@ -181,93 +178,4 @@ public class ForecastFragment extends Fragment {
         return highLowStr;
     }
 
-    private class FetchWeatherTask extends AsyncTask<String, Void, Void> {
-        final String QUERY_PARAM = "q";
-        final String FORMAT_PARAM = "mode";
-        final String UNITS_PARAM = "units";
-        final String DAYS_PARAM = "cnt";
-        final String APPID_PARAM = "appid";
-
-        HttpURLConnection urlConnection;
-        BufferedReader bufferedReader;
-        String forecastJsonStr;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            waitData.setVisibility(View.VISIBLE);
-            listviewForecast.setVisibility(View.GONE);
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            try {
-                Uri uri = Uri.parse(getBaseUrl()).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, params[0])
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(DAYS_PARAM, days)
-                        .appendQueryParameter(UNITS_PARAM, unitType)
-                        .appendQueryParameter(APPID_PARAM, getAppId()).build();
-
-                URL url = new URL(uri.toString());
-
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuilder stringBuilder = new StringBuilder();
-
-                if (inputStream != null) {
-                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line + "\n");
-                    }
-
-                    if (stringBuilder.length() > 0) {
-                        forecastJsonStr = stringBuilder.toString();
-                    }
-                }
-
-            } catch (IOException e) {
-                Log.e("Bima", "Error ", e);
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-
-                if (bufferedReader != null) {
-                    try {
-                        bufferedReader.close();
-                    } catch (IOException e) {
-                        Log.e("Bima", "Error closing stream", e);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            waitData.setVisibility(View.GONE);
-            listviewForecast.setVisibility(View.VISIBLE);
-            try {
-                String[] data = getWeatherDataFromJson(forecastJsonStr, 7);
-                ArrayList<String> foreCastArray = new ArrayList<>(Arrays.asList(data));
-                arrayAdapter = new ArrayAdapter<>(
-                        getActivity(),
-                        R.layout.list_item_forecast,
-                        R.id.list_item_forecast_textview,
-                        foreCastArray);
-
-                listviewForecast.setAdapter(arrayAdapter);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
